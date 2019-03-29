f525982d3dc0b0f229be8361686351bac54f293c
Hain Luud <hain.luud@eesti.ee>
2019-03-29 17:30:46 +0200

    Katkine kood - here you go

--- src/Peaklass.java
+++ src/Peaklass.java
@@ -1,5 +1,6 @@
 import java.io.IOException;
 import java.util.List;
+import java.util.Scanner;
 
 public class Peaklass {
 
@@ -15,9 +16,37 @@ public class Peaklass {
 
     public static void main(String[] args) throws IOException {
         //Kutsub GUI välja
-        GUI.main(new String[]{});
+        //GUI.main(new String[]{});
         List<Isik> isikud = Sisselugeja.iskuteLugeja("isikuandmed.txt");
         List<Üksus> üksused = Sisselugeja.üksusteLugeja("üksused.txt");
+
+// Suhtlus kasutajaga terminali kaudu
+        System.out.println("Teretulemast kasutama KV uut innovaatilist formeerumistarkvara.");
+        System.out.println("Mida soovite teha?");
+        Scanner input = new Scanner(System.in);
+        while (true){
+            System.out.println("\nLisada isik formeerimiseks (1)");
+            System.out.println("Printida senise formeerumise aruanne (2)");
+            System.out.println("Väljuda programmist (3)");
+            int valik = input.nextInt();
+            switch (valik){
+                case 1:{
+                    System.out.println("Palun sisestage oma isikukood: ");
+                    String isikukood = input.next();
+                    System.out.println("sisestasite isikukoodi " + isikukood);
+                    if (kontrolliIsik(isikukood, isikud)){
+                        // TODO: 29/03/2019 registreeri isik
+                        System.out.println("Registreerisin isiku");
+                    }
+                    else System.out.println("Sisestatud isikukoodiga inimest ei leidu meie andmebaasis.");
+                }
+                case 2: // TODO: 29/03/2019 Väljasta registreerunud isikutega auranne ;
+                    System.out.println("Säh sulle tabelit");
+                case 3: System.exit(0);
+            }
+        }
+
+
 
         /*
         for (Isik isik:isikud) {
